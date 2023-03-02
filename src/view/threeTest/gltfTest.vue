a6c6b092ec6986b16ad5ad9076c220cd139f84d8
v-xmengyun <v-xmengyun.gd@chinatelecom.cn>
2023-03-02 14:06:50 +0800

    大型gltf提交

---
+++ src/view/threeTest/gltfTest.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="modelTest">
+  <modelTest url="./Draco/modelDracotest.gltf" key="./Draco/modelDracotest.gltf"></modelTest>
+  <modelTest url="./szyq.glb" key="./szyq.glb"></modelTest>
+  <modelTest url="./model.gltf" key="./model.gltf"></modelTest>
+</div>
+</template>
+
+<script>
+import modelTest  from "@/view/threeTest/modelTest.vue";
+export default {
+  name: "gltfTest.vue",
+  components: {
+    modelTest
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.modelTest{
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: flex-start;
+  flex-wrap: nowrap;
+}
+</style>
